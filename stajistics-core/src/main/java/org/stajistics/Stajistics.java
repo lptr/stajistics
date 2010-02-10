d4b8bbca942f4b8b468446433350956d3593e6b8
lptr11 <lptr11@localhost>
2010-02-10 12:10:32 +0000

    Cleaned up POMs. Moved properties in library.properties into JAR manifests.

--- stajistics-core/src/main/java/org/stajistics/Stajistics.java
+++ stajistics-core/src/main/java/org/stajistics/Stajistics.java
@@ -14,9 +14,6 @@
  */
 package org.stajistics;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * Provides information on the Stajistics API.
@@ -29,39 +26,14 @@ public final class Stajistics {
     protected static final String PROP_NAME = "library.name";
     protected static final String PROP_VERSION = "library.version";
 
-    private static String name;
-    private static String version;
-    static {
-        try {
-            loadProperties(PROPS_FILE);
-        } catch (IOException e) {
-            throw new ExceptionInInitializerError(e);
-        }
+    private static final String name = replaceNull(Stajistics.class.getPackage().getImplementationTitle(), "Stajistics");
+    private static final String version = replaceNull(Stajistics.class.getPackage().getImplementationTitle(), "0.0");
+    
+    private static String replaceNull(String value, String defaultValue) {
+    	return value == null ? defaultValue : value;
     }
 
     private Stajistics() {}
-
-    protected static void loadProperties(final String fileName) throws IOException {
-        Properties props = new Properties();
-
-        ClassLoader classLoader = Stajistics.class.getClassLoader();
-        InputStream in = classLoader.getResourceAsStream(fileName);
-
-        String name = null;
-        String version = null;
-        
-        if (in != null) {
-            props.load(in);
-
-            name = props.getProperty(PROP_NAME);
-            version = props.getProperty(PROP_VERSION);
-        }
-
-        Stajistics.name = (name == null) ? ""
-                                         : name;
-        Stajistics.version = (version == null) ? ""
-                                               : version;
-    }
 
     /**
      * Get the name of the Stajistics API.
