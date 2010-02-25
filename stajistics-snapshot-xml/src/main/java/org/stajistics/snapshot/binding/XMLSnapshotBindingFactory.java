fa0ba8c9a4d4c91ecdd4692df763129feaa7abdc
Troy Kinsella <troy.kinsella@gmail.com>
2010-02-25 23:37:57 +0000

    Updated license copyright years for remaining non-core modules.

--- stajistics-snapshot-xml/src/main/java/org/stajistics/snapshot/binding/XMLSnapshotBindingFactory.java
+++ stajistics-snapshot-xml/src/main/java/org/stajistics/snapshot/binding/XMLSnapshotBindingFactory.java
@@ -1,4 +1,4 @@
-/* Copyright 2009 The Stajistics Project
+/* Copyright 2009 - 2010 The Stajistics Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
